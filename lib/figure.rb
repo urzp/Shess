550572d5439016ccfdb1e7071d5f19cf00a5e1ba
urzp <urzp002@mail.ru>
2016-07-24 21:01:18 +0300

    declare figures classes

---
+++ lib/figure.rb
@@ -0,0 +1,60 @@
+class Figure
+
+  def initialize
+  end
+
+end
+
+class Queen < Figure
+
+  def initialize(color)
+    @color = color
+    @position
+  end
+
+end
+
+class King < Figure
+
+  def initialize(color)
+    @color = color
+    @position
+  end
+
+end
+
+class Rook < Figure
+
+  def initialize(color)
+    @color = color
+    @position
+  end
+
+end
+
+class Bishop < Figure
+
+  def initialize(color)
+    @color = color
+    @position
+  end
+
+end
+
+class Knight < Figure
+
+  def initialize(color)
+    @color = color
+    @position
+  end
+
+end
+
+class Pawn < Figure
+
+  def initialize(color)
+    @color = color
+    @position
+  end
+
+end
