192b9f3e4dcd83f493e339f6adb550adce5ceed9
urzp <urzp002@mail.ru>
2016-07-25 23:14:44 +0300

    fix the initialize of board class

--- lib/board.rb
+++ lib/board.rb
@@ -9,12 +9,14 @@ class Board
     @figures = @figures + [King.new(:white), Queen.new(:black), Rook.new(:black), Rook.new(:black), Bishop.new(:black), Bishop.new(:black), Knight.new(:black), Knight.new(:black)]
     @figures = @figures + [Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black)]
 
+    x = 0
     "A".upto("H") do |char|
-      1.upto(8) do |number|
-        name = "#{char}#{number}"
-        position = [char, number]
+      x = x + 1
+      1.upto(8) do |y|
+        name = "#{char}#{y}"
+        position = [x, y]
         figure = @figures.select{|fig| fig.position == position }
-        figure = nil if figure = []
+        figure = nil if figure == []
         node = Node.new(name, position, figure)
         @board << node
       end
