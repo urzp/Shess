a4d93b06fb14e59a1c873ffeee449736762705f2
urzp <urzp002@mail.ru>
2016-07-26 11:13:53 +0300

    create #draw for board class

--- lib/board.rb
+++ lib/board.rb
@@ -6,13 +6,14 @@ class Board
 
     @figures =            [King.new(:white), Queen.new(:white), Rook.new(:white), Rook.new(:white), Bishop.new(:white), Bishop.new(:white), Knight.new(:white), Knight.new(:white)]
     @figures = @figures + [Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white)]
-    @figures = @figures + [King.new(:white), Queen.new(:black), Rook.new(:black), Rook.new(:black), Bishop.new(:black), Bishop.new(:black), Knight.new(:black), Knight.new(:black)]
+    @figures = @figures + [King.new(:black), Queen.new(:black), Rook.new(:black), Rook.new(:black), Bishop.new(:black), Bishop.new(:black), Knight.new(:black), Knight.new(:black)]
     @figures = @figures + [Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black)]
 
-    x = 0
-    "A".upto("H") do |char|
-      x = x + 1
-      1.upto(8) do |y|
+
+    1.upto(8) do |y|
+      x = 0
+      "A".upto("H") do |char|
+         x = x + 1
         name = "#{char}#{y}"
         position = [x, y]
         figure = @figures.select{|fig| fig.position == position }
@@ -30,6 +31,29 @@ class Board
   end
 
   def draw
+    index = 0
+    1.upto(8) do |y|
+      x = 0
+      line = ""
+      "A".upto("H") do |char|
+        x = x + 1
+        cell = @board[index].figure
+        cell = cell[0].symbol if cell != nil
+        cell = "   " if cell == nil
+        line = line + "\u2502" + cell
+        index = index + 1
+      end
+      if y == 1
+        puts "  \u250C\u2500\u2500\u2500\u252C\u2500\u2500\u2500\u252C\u2500\u2500\u2500\u252C\u2500\u2500\u2500\u252C\u2500\u2500\u2500\u252C\u2500\u2500\u2500\u252C\u2500\u2500\u2500\u252C\u2500\u2500\u2500\u2510"
+      else
+        puts "  \u251C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2524"
+      end
+      puts "#{y} #{line}\u2502"
+      if y == 8
+        puts "  \u2514\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2518"
+        puts "    A   B   C   D   E   F   G   H "
+      end
+    end
   end
 
 end
