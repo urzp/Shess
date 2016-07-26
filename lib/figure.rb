ce9a03e609cf453c79c01295b6c5f62314abea93
urzp <urzp002@mail.ru>
2016-07-26 17:04:49 +0300

    exchange color figures for black background

--- lib/figure.rb
+++ lib/figure.rb
@@ -14,10 +14,10 @@ class Queen < Figure
     @color = color
 
     if @color == :white
-      @symbol = "\u2654"
+      @symbol = "\u265A"
       @position = [4,8]
     else
-      @symbol = "\u265A"
+      @symbol = "\u2654"
       @position = [5,1]
     end
   end
@@ -30,10 +30,10 @@ class King < Figure
     @color = color
 
     if @color == :white
-      @symbol = "\u2655"
+      @symbol = "\u265B"
       @position = [5,8]
     else
-      @symbol = "\u265B"
+      @symbol = "\u2655"
       @position = [4,1]
     end
   end
@@ -51,11 +51,11 @@ class Rook < Figure
     @@count_white = 0 if @@count_white == 2
     if @color == :white
       @@count_white += 1
-      @symbol = "\u2656"
+      @symbol = "\u265C"
       @position = [1,8] if @@count_white == 1
       @position = [8,8] if @@count_white == 2
     else
-      @symbol = "\u265C"
+      @symbol = "\u2656"
       @@count_black += 1
       @position = [1,1] if @@count_black == 1
       @position = [8,1] if @@count_black == 2
@@ -83,11 +83,11 @@ class Bishop < Figure
       @@count_white = 0 if @@count_white == 2
       if @color == :white
         @@count_white += 1
-        @symbol = "\u2657"
+        @symbol = "\u265D"
         @position = [3,8] if @@count_white == 1
         @position = [6,8] if @@count_white == 2
       else
-        @symbol = "\u265D"
+        @symbol = "\u2657"
         @@count_black += 1
         @position = [3,1] if @@count_black == 1
         @position = [6,1] if @@count_black == 2
@@ -115,11 +115,11 @@ class Knight < Figure
       @@count_white = 0 if @@count_white == 2
       if @color == :white
         @@count_white += 1
-        @symbol = "\u2658"
+        @symbol = "\u265E"
         @position = [2,8] if @@count_white == 1
         @position = [7,8] if @@count_white == 2
       else
-        @symbol = "\u265E"
+        @symbol = "\u2658"
         @@count_black += 1
         @position = [2,1] if @@count_black == 1
         @position = [7,1] if @@count_black == 2
@@ -147,10 +147,10 @@ class Pawn < Figure
       @@count_white = 0 if @@count_white == 8
       if @color == :white
         @@count_white += 1
-        @symbol = "\u2659"
+        @symbol = "\u265F" #"\u265F"
         @position = [@@count_white,7]
       else
-        @symbol = "\u265F"
+        @symbol = "\u2659" #"\u2659"
         @@count_black += 1
         @position = [@@count_black,2]
       end
