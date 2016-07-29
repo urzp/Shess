02c476d2c6dc4793b5bcc3e7ee74fd4a18a821fc
urzp <urzp002@mail.ru>
2016-07-29 10:07:11 +0300

    this is tool

--- lib/tool.rb
+++ lib/tool.rb
@@ -1,9 +1,21 @@
 require '../lib/player'
 require '../lib/board'
 require '../lib/figure'
+require '../lib/game'
 
 board = Board.new
+
 board.draw
-player = Human.new(:white)
-print player.turn(board)
-puts
+player_1 = Human.new(:white)
+player_2 = Human.new(:black)
+game = Game.new(player_1, player_2)
+turn = player_1.turn(board)
+game.turn(player_1, turn, board)
+turn = player_2.turn(board)
+game.turn(player_2, turn, board)
+turn = player_1.turn(board)
+game.turn(player_1, turn, board)
+turn = player_2.turn(board)
+game.turn(player_2, turn, board)
+turn = player_1.turn(board)
+game.turn(player_1, turn, board)
