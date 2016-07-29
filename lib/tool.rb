4d9512f5d7870463077be2d86f309aec23fc80f1
urzp <urzp002@mail.ru>
2016-07-29 23:14:47 +0300

    rewrite for abillity to play

--- lib/tool.rb
+++ lib/tool.rb
@@ -9,13 +9,10 @@ board.draw
 player_1 = Human.new(:white)
 player_2 = Human.new(:black)
 game = Game.new(player_1, player_2)
-turn = player_1.turn(board)
-game.turn(player_1, turn, board)
-turn = player_2.turn(board)
-game.turn(player_2, turn, board)
-turn = player_1.turn(board)
-game.turn(player_1, turn, board)
-turn = player_2.turn(board)
-game.turn(player_2, turn, board)
-turn = player_1.turn(board)
-game.turn(player_1, turn, board)
+
+1.upto(20) do
+  turn = player_1.turn(board)
+  game.turn(player_1, turn, board)
+  turn = player_2.turn(board)
+  game.turn(player_2, turn, board)
+end
